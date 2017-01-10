1c67fb6af6112dc93a1e0fe755d9928140c8b683
Bilal Ujede <bujede@gmail.com>
2017-01-10 09:12:29 +0530

    Added User Registration Module.

---
+++ app/src/main/java/asia/bilalujede/qnaforum/UserRegistration.java
@@ -0,0 +1,13 @@
+package asia.bilalujede.qnaforum;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_registration);
+    }
+}
---
+++ app/src/main/java/asia/bilalujede/qnaforum/background_db_work.java
@@ -0,0 +1,27 @@
+package asia.bilalujede.qnaforum;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Bilal Ujede on 09-01-2017.
+ */
+
+public class background_db_work extends AsyncTask<Void, Void, Void>{
+
+    @Override
+    protected void onPreExecute(){
+
+    }
+    @Override
+    protected Void doInBackground(Void... params) {
+        return null;
+    }
+    @Override
+    protected void onProgressUpdate(Void... aVoid){
+
+    }
+    @Override
+    protected void onPostExecute(Void aVoid){
+
+    }
+}
